1217c5755e4d3f8f04b63e503cfb59234cc797c0
Tong Yu <rachieyu@gmail.com>
2018-07-09 11:17:03 +1000

    added solution for printing indexs of non-unique keys for 709 treasure hunter

--- 709-treasure-hunter/treasure-hunter.js
+++ 709-treasure-hunter/treasure-hunter.js
@@ -1,3 +1,4 @@
+//https://gist.github.com/kasun-maldeni/f0aaae72a93520c7ecabf2c9ea6faefe
 console.log('treasure hunter!');
 var map1 = [
     ["A","A","A","A","A"],
@@ -64,4 +65,37 @@ function treasureHunter(map,key){
     }
 }
 
-console.log(treasureHunter(holyGrailMap,'Z'));
+console.log(treasureHunterPlus(map3,'v'));
+
+//if you also want to console.log() all the locations of non-unique key
+function treasureHunterPlus(map,key){
+    var Y=[];
+    var X=[];
+    var line=[];
+    map.forEach(element => {
+        for(k=0;k<element.length;k++){
+            if (element[k] === key){
+                X.push(k);
+                line.push(element);
+            }
+        }
+        for(var i=0;i<line.length;i++){
+            if (element===line[i]){
+                Y.push(map.indexOf(line[i]));
+            }
+        }
+    })
+    console.log(line);
+    console.log(X);
+    console.log(Y);
+    if (X.length === 1){
+        return [Y[0],X[0]];    
+    } else if (X.length > 1){
+        for (var j=0;j<line.length;j++){
+            console.log([Y[j],X[j]])
+        }
+        return 'that doesnt belong in a museum';
+    } else {
+        return 'cant find the treasure';
+    }
+}
