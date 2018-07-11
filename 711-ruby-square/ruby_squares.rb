bd38cad737c32b3815c2aaa05daa3374f39ed548
Tong Yu <rachieyu@gmail.com>
2018-07-11 10:38:36 +1000

    added select method for ruby square

--- 711-ruby-square/ruby_squares.rb
+++ 711-ruby-square/ruby_squares.rb
@@ -1,4 +1,4 @@
-def get_squares arr
+def get_squares_one arr
     new_arr =[]
     arr.each do |num|
         if (num ** 0.5) % 1 == 0
@@ -7,20 +7,16 @@ def get_squares arr
     end
     new_arr.sort!.uniq!
 end
-p get_squares ([4, 1, 16, 1, 10, 35, 22])
+p get_squares_one ([4, 1, 16, 1, 10, 35, 22])
 
 
-# def get_square arr
-#     arr.each do |num|
-#         if (num ** 0.5) % 1 != 0
-#             arr.delete num
-#             p arr
-#         end
-#     end
-#     arr.sort!.uniq!
-# end
-# p get_square ([4, 1, 16, 1, 10, 35, 22])
+def get_squares_two arr
+    new_arr = arr.select {|num| (num ** 0.5) % 1 == 0}
+    new_arr.sort!.uniq!
+end
+p get_squares_two ([4, 1, 16, 1, 10, 35, 22])
 
-# [4, 1, 16, 1, 35, 22]
-# [4, 1, 16, 1, 35]
-# [1, 4, 16, 35]
+def get_squares_three arr
+    arr.select {|num| num ** 0.5 % 1 == 0}.sort!.uniq!
+end
+p get_squares_three [4, 1, 16, 1, 10, 35, 22]
