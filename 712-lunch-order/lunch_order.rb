547b960cd71262f7f5cff0c6a6f5278dc271e740
Tong Yu <rachieyu@gmail.com>
2018-07-12 11:18:52 +1000

    added warmup 712 ruby hashes luch-orders

---
+++ 712-lunch-order/lunch_order.rb
@@ -0,0 +1,26 @@
+# https://gist.github.com/kasun-maldeni/3ff70009c99feb615a717a9a4ba07b4d
+
+def lunch_orders 
+    answer = "y"
+    orders = {}  
+        while answer == "y"
+        puts "name for order"
+        name = gets.chomp
+        puts "what would you like to order"  
+            if orders[name] == nil
+            orders[name]=[].push(gets.chomp)
+            else orders[name].push(gets.chomp)
+            end
+        puts "something else to add? y/n"
+        answer = gets.chomp
+        end
+    orders.each do |name,food| 
+        if food.length ==1
+        puts "#{name} orderd #{food.last}" 
+        else puts "#{name} orderd #{food[0..-2]*', '} & #{food.last}" 
+        end
+    end
+end
+lunch_orders()
+
+
